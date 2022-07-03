971b0c42eba303da40f9d7ab601ce38f3f3a6684
keerthanajayabhaskaran <102846345+keerthanajayabhaskaran@users.noreply.github.com>
2022-07-03 22:47:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,12 @@ with left_column:
 input_Length1 = st.slider('Vertical length(cm)', 0.0, max(data["Length1"]), 1.0)
 input_Length2 = st.slider('Diagonal length(cm)', 0.0, max(data["Length2"]), 1.0)
 #input_Length3 = st.slider('Cross length(cm)', 0.0, max(data["Length3"]), 1.0)
-input_Length3 = 24.97
+
 input_Height = st.slider('Height(cm)', 0.0, max(data["Height"]), 1.0)
 #input_Width = st.slider('Diagonal width(cm)', 0.0, max(data["Width"]), 1.0)
-input_Width = 3.65785
+input_Width = input_Height/2
+input_Length3 = input_Length2*1.099869387
+
 
 
 if st.button('Predict Fish Weight'):
